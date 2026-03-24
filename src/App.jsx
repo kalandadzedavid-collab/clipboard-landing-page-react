5152eeba0dd4fab713b0f706367e19899d5b1fbd
student <student@local.emis.ge>
2026-03-24 15:34:54 +0400

    header, footer and button ready

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Button from "./components/Button";
+
+export default function App() {
+  return <>
+  
+    <Header />
+  <Button />
+
+    <Footer />
+  </>;
+}
---
+++ src/components/Button.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Button = () => {
+  return (
+    <button
+      className=" w-80 h-14 bg-teal-500 rounded-3xl shadow-[0px_10px_20px_0px_rgba(137,229,199,0.50)] shadow-[inset_0px_-3px_0px_0px_rgba(0,0,0,0.15)]
+    text-white
+text-lg
+font-semibold
+font-['Bai_Jamjuree']
+leading-8
+tracking-wide
+    hover:cursor-pointer
+    hover:bg-teal-500/75
+    "
+    >
+      test button
+    </button>
+  );
+};
+
+export default Button;
